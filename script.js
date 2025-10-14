f04c5dd4f6d78092d13f3c657554f3172b7ae74b
Nick Farley <educationstation64@gmail.com>
2025-10-14 18:01:41 -0400

    subdomains 1.0

--- script.js
+++ script.js
@@ -3,5 +3,20 @@ document.addEventListener("DOMContentLoaded", function(){
     .then(response => response.text())
     .then(data => {
         document.getElementById("sidebar-container").innerHTML = data;
+        activateCurrentMenuItem();
     })
 });
+
+function activateCurrentMenuItem() {
+    const currentPage = window.location.pathname.split("/").pop();
+    const sidebarLinks = document.querySelectorAll('#sidebar-container a');
+    sidebarLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPage){
+            link.classList.add('current-page');
+            const parentSubmenu = link.closest('.submenu');
+            if (parentSubmenu) {
+                parentSubmenu.parentElement.classList.add('active');
+            }
+        }
+    });
+}
