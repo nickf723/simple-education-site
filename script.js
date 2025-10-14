b57c69c0ac490bcca68f0a1a6c37f63a14d92210
Nick Farley <educationstation64@gmail.com>
2025-10-14 18:36:04 -0400

    sidebar bugs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    fetch("sidebar.html")
+    fetch("/sidebar.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("sidebar-container").innerHTML = data;
